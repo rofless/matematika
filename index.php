4e51c0b3eaae9aa017668dd9da7d1d31b813f986
rofless <a.stazinskij@gmail.com>
2018-03-19 12:20:22 +0200

    Initial comit

---
+++ index.php
@@ -0,0 +1,43 @@
+
+<?php
+include ('fukncijos.php')
+?>
+<?php
+
+
+echo suma(1, 20)."<br>";
+
+echo dalyba (1, 20)."<br>";
+
+echo daugyba (1, 20)."<br>";
+
+echo $vardas;
+$amzius=21;
+if ($amzius>=21) {
+    echo $amzius;
+}
+else {
+    echo "nepilnametis";
+}
+
+echo reverse ('labaggggs')."<br>";
+
+echo Palindrome ("labas")."<br>";
+
+picture(7)."<br>";
+
+echo getChineseZodiac(5)."<br>";
+
+?>
+
+<?php
+
+
+for ($string=$length; $string>=0; $string--);
+{
+    echo substr("marius", $length="0");
+}
+?>
+
+
+
